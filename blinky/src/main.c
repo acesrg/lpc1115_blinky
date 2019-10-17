999a4d12b53009cac8422aee9682340c8e7fb290
MarcoMiretti <marco.miretti@gmail.com>
2019-10-17 14:54:23 -0300

    initial commit with blinky project

---
+++ blinky/src/main.c
@@ -0,0 +1,32 @@
+/**
+  **********************************************************************
+  * @file    main.c
+  * @author  Marco Miretti https://github.com/MarcoMiretti
+  * @brief   Code used to turn a built in led on and off
+  *
+  *	Just a blinky
+  *
+  **********************************************************************
+  * @section LICENSE TODO: choose license
+  **********************************************************************
+  */
+
+#include "main.h"				/* CMSIS is included in header */
+
+int main(void)
+{
+	LPC_IOCON->PIO0_7 &= ~0x01;		/* Pin function PIO0_7 */
+	LPC_IOCON->PIO0_7 &= ~0x18;		/* MODE inactive (NOPULL) */
+
+	LPC_GPIO0->DIR	|= 0x80;		/* GPIO0_7 as output */
+	LPC_GPIO0->DATA |= 0x80;		/* GPIO0_7 data = 1 */
+	while(1)
+	{
+		volatile unsigned int i =0;
+		for(i=0; i<1000000; ++i);	/* Not so fancy delay*/
+		LPC_GPIO0->DATA &= ~0x80;	/* GPIO0_7 data = 0 */	
+		for(i=0; i<1000000; ++i);
+		LPC_GPIO0->DATA |= 0x80;
+	}
+	return 0;
+}
